9df0785645c2cd2d02a351e9dc382cd4eb748966
Ahmad Raza Khokhar <ark@Ahmads-MacBook-Air.local>
2026-07-26 17:35:46 +0500

    Add js basics

---
+++ javascript/arrays/index.js
@@ -0,0 +1,17 @@
+// Arrays in JS
+
+const fruites = ["Apple", "Mango", "Pineapple"];
+
+console.log(fruites)
+
+let array = [100, true, "Ahmad"];
+
+console.log(array)
+
+const sampleArray = [
+    {name:"Ahmad", age:24},
+    {name:"Ali", age:18},
+    {name:"Jumshed", age:22},
+]
+
+console.log(sampleArray)
---
+++ javascript/variables/index.js
@@ -0,0 +1,8 @@
+// Variable in JS
+
+let myFullName = "Ali Raza";
+var classAndSection = "10th - Section G";
+const age = 19;
+
+console.log(`My name is ${myFullName}, I study in ${classAndSection} and my age is ${age}`)
+
