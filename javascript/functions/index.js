82ababd3d55213964c1470f4678af9ce659aa65f
Ahmad Raza Khokhar <ark@Ahmads-MacBook-Air.local>
2026-07-26 17:35:46 +0500

    Add js basics

---
+++ javascript/functions/index.js
@@ -0,0 +1,17 @@
+// Functions in JS
+
+function PrintMyName(){
+    console.log("My name is Ahmad Raza.")
+}
+// Executing function
+/*
+Function in Javascript is a type of object that allows us to re-use 
+the logic anywhere in the project/file.
+*/
+PrintMyName();
+
+const PrintMyEducation = () => {
+    console.log("I study in 10th standard's section E.")
+}
+
+PrintMyEducation()
---
+++ javascript/objects/index.js
@@ -0,0 +1,28 @@
+// Objects in JS
+
+let studentProfile = {
+  name: "Ahmad Raza Khokhar",
+  class: "12th",
+  section: "E",
+  age: 24,
+};
+
+console.log(studentProfile)
+
+
+// Nested Objects
+
+const customer = {
+    name:"Ali",
+    phone:"+9231423232",
+    adult: true,
+    address: {
+        streetAddressLine1:"DHA PH-2, Sector Z, Lahore.",
+        streetAddressLine2:"House No. 200, Sector Z, DHA Phase 2",
+        location:{
+            lat:200,
+            long: 150
+        }
+    },
+    reviews: [5, 3, 4.5, 3.4]
+}
